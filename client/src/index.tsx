3dad69087fae22f7b62f21ff85b310e22ba9e0f9
Letsgetthismoney <100106167+Letsgetthismoney@users.noreply.github.com>
2024-11-01 11:10:32 +0100

    Update index.tsx to base name in browser router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -72,7 +72,7 @@ store.dispatch(setSelectedPage(1))
 
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename="/ims">
           <Provider store={store}>
               <MantineProvider theme={theme} defaultColorScheme="dark" >
                   <Notifications position="top-right"/>
